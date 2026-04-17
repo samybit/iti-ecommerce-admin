812afc12dae7d15c34bc4f3a8194421add5e6c54
Shimaa Ameen <shimaaameen976@gmail.com>
2026-04-17 12:04:12 +0200

    fix add category

--- src/app/(dashboard)/categories/add/page.tsx
+++ src/app/(dashboard)/categories/add/page.tsx
@@ -1,9 +1,37 @@
-import CategoryForm from "@/components/categories/CategoryForm";
+"use client";
 
-export default function AddPage() {
+import CategoryForm from "@/components/categories/CategoryForm";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function AddCategoryPage() {
+
+  const router = useRouter();
+
+  const [form, setForm] = useState({
+    name: "",
+  });
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    await fetch("/api/categories", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(form),
+    });
+
+    router.push("/categories");
+    router.refresh();
+  };
+
   return (
-    <div className="p-6">
-      <CategoryForm />
-    </div>
+    <CategoryForm
+      form={form}
+      setForm={setForm}
+      onSubmit={handleSubmit}
+    />
   );
 }
